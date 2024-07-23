bc15457b162ef1470a2efab3ecb0b992ab2ffa11
veerateja <earadhyulaveerateja03@gmail.com>
2024-07-23 21:09:34 +0530

    error boundary and pre loading page

---
+++ src/BootStrap.jsx
@@ -0,0 +1,33 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import UserContextProvider from "./context/UserContext/UserContextProvider.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import App from "./App.jsx";
+export default function BootStrap() {
+  const queryClient = new QueryClient();
+  const theme = createTheme({
+    palette: {
+      // mode: "dark",
+      primary: {
+        main: "#006A4D",
+      },
+      secondary: {
+        main: "#e87a00",
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <UserContextProvider>
+          <Router>
+            <CssBaseline />
+            <App />
+          </Router>
+        </UserContextProvider>
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
+}
